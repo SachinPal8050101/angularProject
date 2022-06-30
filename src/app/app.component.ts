c1317ad92d51f30f76acb52221b6456d3ca8c6a7
Sachin Kumar <sachin.kumar1@daffodilsw.com>
2022-06-30 14:26:33 +0530

    add

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,8 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'angularProject';
+  bestMovieEver= false;
+  onClick(event: any){
+    console.log('click',event);
+  }
 }
